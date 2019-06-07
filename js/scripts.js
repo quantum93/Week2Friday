cdd6e3ce764bd76b0ddf810fa1367332bc887f3d
Tae Lee <taebumlee@gmail.com>
2019-06-07 13:30:30 -0700

    This is hide and show action using javascript and branch at first

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,16 @@
 $(document).ready(function () {
   $('.formOne').submit(function (event) {
     event.preventDefault();
-    var choice = $('#questionOne').val();
-    if (choice === '0') {
-      $('#languageOne').show();
+    var choiceOne = $('#questionOne').val();
+    var choiceTwo = $('#questionTwo').val();
+    var choiceThree = $('#questionThree').val();
+    var choiceFour = $('#questionFour').val();
+    var choiceFive = $('#questionFive').val();
+
+    if (choice0 === '0') {
+      $('#languageZero').show();
+    } else if (choice0 === '1' && choice) {
+
     }
 
   });
